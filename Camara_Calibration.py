5d15a7d15970beb3f7a5326caa84f99aaf8650ef
JhonHader <45322492+JhonHader@users.noreply.github.com>
2020-10-26 22:27:19 -0500

    Commented

--- Camara_Calibration.py
+++ Camara_Calibration.py
@@ -7,16 +7,19 @@ import json
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+# Define number of corners (width, height)
 width = 7
 height = 7
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((height * width, 3), np.float32)
 objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
+
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
 
-path = input('Ingrese ruta de la carpeta con las imagenes: ')#r'.\PC_images' #r'.\Phone_images'
+path = input('Ingrese ruta de la carpeta con las imagenes: ') #r'.\PC_images' #r'.\Phone_images'
 path_file = os.path.join(path, 'image*.jpg')
 
 images = glob.glob(path_file)
@@ -24,6 +27,7 @@ print(images)
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
     # Find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
 
